b6b1aa94fe0731c8123c35e446c7e25efda30a77
Haykal <haikelbrinis@gmail.com>
2019-04-05 13:20:50 +0200

    add project angular

--- front-product-management/src/app/app-routing.module.ts
+++ front-product-management/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { NewProductComponent } from './product-list/new-product/new-product.component';
+import { EditProductComponent } from './product-list/edit-product/edit-product.component';
+import { SingleProductComponent } from './product-list/single-product/single-product.component';
+const routes: Routes = [
+  { path: 'product/list', component: ProductListComponent },
+  { path: 'product/new', component: NewProductComponent },
+  { path: 'product/edit/:id', component: EditProductComponent },
+  { path: 'product/single/:id', component: SingleProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-product-management/src/app/app.module.ts
+++ front-product-management/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -20,7 +21,10 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
